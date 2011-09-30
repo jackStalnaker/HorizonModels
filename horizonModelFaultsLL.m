3a1972ef7fb9fb9d4013b40838a743c4134fb9bd
Jack Stalnaker <jack.stalnaker@gmail.com>
2011-09-30 13:27:13 -0500

    likelihood of horizon model updated

--- horizonModelFaultsLL.m
+++ horizonModelFaultsLL.m
@@ -12,14 +12,25 @@ function LL = horizonModelFaultsLL(hmean,hstd,hdata)
 % =================
 % LL:           Negative log likelihood
 
-% Likelihood of normal step size is gaussian.
+% NOTE: the following likelihoods are declared as anonymous functions in
+% matlab. Meaning they're all just one line functions that take an argument
+% x as defined in the @(x) part of the code. That's because these values
+% will be integrated over, since we don't have direct observations of the
+% data they represent the likelihood of.
 
+% Likelihood of random walk step size is gaussian. 
+rwL = @(x)(1/sqrt(2*pi*rwStd^2)) * exp((-(x-rwMean).^2)/(2*rwStd.^2));
 
+% Likelihood of a fault being present is a bernoulli distribution.
+% where the probability of a fault being present at all is a poisson
+% estimated number of faults over the number of traces. 
+pFault = nFault/size(hdata,1);
 
-% Likel
+% bernoulli pmf/likelihood
+fpL = @(x)pFault.^x * (1-pFault).^(1-x);
 
+% Likelihood of fault throw is zero-mean gaussian
+ftL = @(x)(1/sqrt(2*pi*ftStd^2)) * exp((-(x).^2)/(2*ftStd.^2));
 
+% numerically integrate for each observation in the data vector hdata
 
-
-
-LL = sum((hdata-hmean).^2)./(2*hstd^2);
